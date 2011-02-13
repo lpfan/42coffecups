371196736dfd380a18917254c0ffde78b238ac62
lpfan <misha.shchetinin@gmail.com>
2011-02-13 23:25:25 +0200

    Ticket:1, Branch:ticket1 Create model, view, fixture and base

--- my_info/views.py
+++ my_info/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.generic.simple import direct_to_template
+from models import MyInfo
+
+def index(request):
+  info = MyInfo.objects.get(pk=1)
+  return direct_to_template(request, "index.html" , {"surname"     : info.surname,
+						     "name"        : info.name,
+						     "bday"        : info.bday,
+						     "contacts"    : info.contacts,
+						     "short_story" : info.short_story
+						    })
