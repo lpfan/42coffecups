2be44590626151400de4f0414e2095c8bcecce0e
lpfan <misha.shchetinin@gmail.com>
2011-03-09 22:22:11 +0200

    Add admin and config urls.py

--- _42cupstest/urls.py
+++ _42cupstest/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
@@ -12,5 +10,9 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT }
+    ),
+    url(r'^', include("tickets.urls")),
 )
