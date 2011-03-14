8f9eb305136c24597e739fd698cdb44d266fbb29
lpfan <misha.shchetinin@gmail.com>
2011-03-14 14:17:13 +0200

    Ticket:6. Add widget to DateField.

--- _42cupstest/tickets/forms.py
+++ _42cupstest/tickets/forms.py
@@ -1,6 +1,19 @@
-from django.forms import ModelForm
+from django import forms
 from models import MyInfo
 
-class EditMyInfoForm(ModelForm):
+class CalendarWidget(forms.TextInput):
+    class Media:
+        css = {
+               'all': ('css/ui-lightness/jquery-ui-1.8.10.custom.css',),
+        }
+        
+        js = ("http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js", 
+              "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js",
+              )
+    
+class EditMyInfoForm(forms.ModelForm):
     class Meta:
         model = MyInfo
+        widgets = {
+                   'bday':CalendarWidget,
+                   }
