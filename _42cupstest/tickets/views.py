f9c786b1bfa16076335a07f622abd5a815944e19
lpfan <misha.shchetinin@gmail.com>
2011-03-15 17:06:59 +0200

    ticket:6 Add ajax-form and datepicker.

--- _42cupstest/tickets/views.py
+++ _42cupstest/tickets/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.simple import direct_to_template
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 
 from models import MyInfo, RequestStore
 from forms import EditMyInfoForm
@@ -23,10 +23,10 @@ def show_settings(request, template_name="show_settings.html"):
 def edit_my_info(request, template_name="editpage.html"):
     info = MyInfo.objects.get(pk=1)
     if request.method == 'POST':
-        form = EditMyInfoForm(request.POST, instance=info)
+        form = EditMyInfoForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect("/contact/")
+            return HttpResponseRedirect('/contact/')
     form = EditMyInfoForm(instance=info)    
     return direct_to_template(request, template_name, {"form":form, "title":"Edit page"})
         
