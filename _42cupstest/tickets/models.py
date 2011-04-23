fb79208006923211b36d22bc47ce1a348abd9e99
lpfan <misha.shchetinin@gmail.com>
2011-04-23 14:16:18 +0300

    ticket:t1_contact Fixing template according to mockup 	modified:   media/css/style.css 	modified:   tickets/fixtures/initial_data.json 	modified:   tickets/models.py 	modified:   tickets/templates/contact.html

--- _42cupstest/tickets/models.py
+++ _42cupstest/tickets/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 class MyInfo(models.Model):
+  
   surname = models.CharField(max_length=100)
   name = models.CharField(max_length=100)
   bday = models.DateField()
-  contacts = models.TextField()
-  short_story = models.TextField()
+  email = models.EmailField()
+  jabber_id = models.CharField(max_length=100)
+  skype_id = models.CharField(max_length=100)
+  other_contacts = models.TextField()
+  bio = models.TextField()
   
   def __unicode__(self):
     return "name: %s, surname: %s" % (self.name, self.surname)
